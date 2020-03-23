687a833f6566658a1da7a04fab14bdd349aeba76
Harout77 <harout@live.ca>
2020-03-23 12:50:41 -0400

    GameOver

---
+++ Pong/Assets/websurvey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class websurvey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void survey()
+    {
+
+        Application.OpenURL("https://docs.google.com/forms/d/1ASOIyyX_zT4u3E3HIyKQTMTh0EV7xmIVwsaEkzWoXSE/prefill");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
